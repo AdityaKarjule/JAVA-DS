1cbe54c3f2a2fe9a5623a319e3253e87f0e661a1
Aditya9909 <66624995+AdityaKarjule@users.noreply.github.com>
2022-07-25 20:44:17 +0530

    Add files via upload

---
+++ s.java
@@ -0,0 +1,26 @@
+ class clothes
+{
+	
+	Shirt(size(),height,gender,lenght,rate)
+	{
+			Shirt s = new Shirt ();
+			return s;
+	}
+}
+
+class Tshirt extends Shirt
+{
+	Tshirt(lenght, height,size,gender,rate) {
+			Tshirt ts = new Tshirt();
+			return ts;
+
+	}
+}
+
+class vneck{ }
+class polo{ }
+class roundneck{ }
+
+class cloth feel {}
+
+
