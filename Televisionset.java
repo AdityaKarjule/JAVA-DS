bea3d880c389fc47fdd5d4ac4cb52c2c1f9b8f8e
Aditya9909 <66624995+AdityaKarjule@users.noreply.github.com>
2022-07-27 23:17:17 +0530

    Add files via upload

---
+++ Televisionset.java
@@ -0,0 +1,79 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+
+
+public class Televisionset {
+	public static void main(String[] args) {
+		
+		System.out.println("----------T.V ON-------------");
+			Television	tl = new Television();
+	try{
+			tl.switchOn();
+			tl.selectChannel();
+		} catch (TelevisionSupplyNotOnException n) {
+			System.out.println("Problem : "+n);
+		}
+		
+		System.out.println("-------------");
+	}
+}
+
+class TelevisionSupplyNotOnException extends Exception
+{
+	TelevisionSupplyNotOnException(String msg) {
+		super(msg);
+	}
+}
+
+class Television
+{
+	boolean current;
+	
+Television() 	{
+		System.out.println("Television is  ready to be binged....but is it ready with power supply ? ?");
+	}
+	
+	void switchOn() {
+		if(current==false) {
+			current = true;
+		}
+		System.out.println("T.V is switched on....");
+		
+		
+	void selectChannel() throws TelevisionSupplyNotOnException
+	{
+		if(current == false ) {
+		TelevisionSupplyNotOnException tlSwitchNotOnEx = new TelevisionSupplyNotOnException("The T.V. is not connected with an external supply.");
+			throw tlSwitchNotOnEx;
+		}
+
+		System.out.println("Channel binged....");
+		binge();
+		System.out.println("Channel changed...");
+	}
+	
+	}
+
+void binge()
+	{
+			for(int i=1;i<=0;i++) {
+				System.out.println("\tWatching a channel."+i);
+			}
+			change();
+	}
+	
+	void change()
+	{
+			for(int i=1;i<=0;i++) {
+				System.out.println("\tChannel is being changed."+i);
+			}
+	}
+	
+		void switchOff() {
+		if(current==true) {
+			current = false;
+		}
+		System.out.println("T.V. is switched off.");
+	}
+}
