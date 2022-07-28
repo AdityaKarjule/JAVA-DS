59c005f070f3cd5c63186307b621b56de99b38d9
Aditya9909 <66624995+AdityaKarjule@users.noreply.github.com>
2022-07-28 09:27:43 +0530

    Add files via upload

--- Televisionset.java
+++ Televisionset.java
@@ -1,45 +1,44 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-
-
 
 public class Televisionset {
 	public static void main(String[] args) {
-		
+
 		System.out.println("----------T.V ON-------------");
-			Television	tl = new Television();
-	try{
-			tl.switchOn();
+		Television tl = new Television();
+		try {
+			//tl.switchOn();
 			tl.selectChannel();
 		} catch (TelevisionSupplyNotOnException n) {
-			System.out.println("Problem : "+n);
+			System.out.println("Problem : " + n);
 		}
-		
+
 		System.out.println("-------------");
 	}
 }
 
-class TelevisionSupplyNotOnException extends Exception
-{
+class TelevisionSupplyNotOnException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	TelevisionSupplyNotOnException(String msg) {
 		super(msg);
 	}
 }
 
-class Television
-{
+class Television {
 	boolean current;
-	
-Television() 	{
+
+	Television() {
 		System.out.println("Television is  ready to be binged....but is it ready with power supply ? ?");
 	}
-	
+
 	void switchOn() {
 		if(current==false) {
 			current = true;
 		}
 		System.out.println("T.V is switched on....");
-		
+	}
 		
 	void selectChannel() throws TelevisionSupplyNotOnException
 	{
@@ -53,25 +52,23 @@ Television() 	{
 		System.out.println("Channel changed...");
 	}
 	
+	
+
+	void binge() {
+		for (int i = 1; i <= 0; i++) {
+			System.out.println("\tWatching a channel." + i);
+		}
+		change();
 	}
 
-void binge()
-	{
-			for(int i=1;i<=0;i++) {
-				System.out.println("\tWatching a channel."+i);
-			}
-			change();
+	void change() {
+		for (int i = 1; i <= 0; i++) {
+			System.out.println("\tChannel is being changed." + i);
+		}
 	}
-	
-	void change()
-	{
-			for(int i=1;i<=0;i++) {
-				System.out.println("\tChannel is being changed."+i);
-			}
-	}
-	
-		void switchOff() {
-		if(current==true) {
+
+	void switchOff() {
+		if (current == true) {
 			current = false;
 		}
 		System.out.println("T.V. is switched off.");
